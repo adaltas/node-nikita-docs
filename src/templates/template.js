2bff43ef1700f6a7f9cc3f0df567ff4364cd1c9a
David Worms <david@adaltas.com>
2018-03-29 22:50:42 +0200

    template: simplify html wrapping

--- src/templates/template.js
+++ src/templates/template.js
@@ -9,14 +9,10 @@ export default function Template({
   const { frontmatter, html } = markdownRemark;
   return (
     <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.description}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
+      <div
+        className="blog-post-content"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </div>
   );
 }
