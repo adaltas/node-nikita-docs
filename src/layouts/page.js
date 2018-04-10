19591c78f2dd063fb818754ebadbb857215477c4
David Worms <david@adaltas.com>
2018-04-10 14:56:51 +0200

    package: remove mixme

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import mixme from 'mixme'
 
 import 'typeface-roboto'
 import { withStyles } from 'material-ui/styles';
